a602ea3975c3ac4d61ee72d3bc946ca8ab991019
Paulo Vitor Lopes Meira <pvitor-l@c2r2p5.42sp.org.br>
2024-10-14 21:12:46 -0300

    files adds isprint.c

---
+++ ft_isalnum.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 20:00:36 by pvitor-l          #+#    #+#             */
+/*   Updated: 2024/10/14 20:37:23 by pvitor-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_isdigit(int c);
+
+int	ft_isalpha(int c);
+
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
