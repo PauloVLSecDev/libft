3735b6f94f567c2acb0bdd627f4a9e18be72a2b1
Paulo Vitor Lopes Meira <pvitor-l@c3r2p2.42sp.org.br>
2024-10-23 20:01:19 -0300

    memmove complet

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 19:55:52 by pvitor-l          #+#    #+#             */
-/*   Updated: 2024/10/23 19:12:42 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2024/10/23 19:59:41 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,20 @@ void	*ft_memmove(void *dest, void *str, size_t len)
 	i = 0;
 	temp_d = (unsigned char *) dest;
 	temp_s = (const unsigned char *) str;
-	if (temp_d == NULL && temp_d == NULL)
-	{
-		return (NULL);
-	}
-	while (len > i)
-	{
-		temp_d[i] = temp_s[i];
-		i++;
-	}
 	if ((temp_d > temp_s) && temp_d < (temp_s + len))
 	{
 		while (len > 0)
 		{
 			temp_d[len - 1] = temp_s[len - 1];
 			len--;
+		}
+	}
+	else
+	{
+		while (len > i)
+		{
+			temp_d[i] = temp_s[i];
+			i++;
 		}
 	}
 	return (dest);
