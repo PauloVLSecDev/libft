a0697dd3cc02df84ad624822d2a3a30dd30caf4c
Paulo Vitor Lopes Meira <pvitor-l@student.42sp.org.br>
2024-11-27 19:30:38 -0300

    current i m bad

---
+++ ft_istadd_front.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_istadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 16:40:18 by pvitor-l          #+#    #+#             */
+/*   Updated: 2024/11/27 19:29:27 by pvitor-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_istadd_front(t_list **ist, t_list *new)
+{
+	*ist = head;
+		//tenho que entender esta merda uma hora que raiva disso
+	
+}
